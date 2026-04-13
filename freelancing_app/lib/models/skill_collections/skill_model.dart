b8044f686d5445fb481720509125956b376943cf
huda <dodezhh@hotmail.com>
2026-04-13 07:40:56 +0300

    fix account info

--- freelancing_app/lib/models/skill_collections/skill_model.dart
+++ freelancing_app/lib/models/skill_collections/skill_model.dart
@@ -1,22 +1,26 @@
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 
 class SkillModel {
+  
+  final String id;
   final String name;
   final List<String> subSkills;
   RxBool expanded = false.obs;
 
   // قائمة المهارات الفرعية المختارة
   RxList<String> selectedSubSkills = <String>[].obs;
-   SkillModel({
-    required this.name,
-    required this.subSkills,
-    
-  });
 
-  factory SkillModel.fromMap(Map<String, dynamic> data) {
+  SkillModel({
+    required this.id,
+    required this.name,
+    this.subSkills = const [],
+  });
+ 
+   factory SkillModel.fromMap(Map<String, dynamic> map, String docId) {
     return SkillModel(
-      name: data['name'] ?? '',
-      subSkills: List<String>.from(data['subSkills'] ?? []),
+      id: docId,
+      name: map['categoryName'] ?? '',
+      subSkills: List<String>.from(map['skills'] ?? []),
     );
   }
 }
